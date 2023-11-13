93d7ffa5f446f7a1ac7c8eedbce3b32f2554ae65
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-11-12 19:26:43 -0800

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,66 +1,58 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _printf - a function that prints everything
- * @format: the string to print
- * Return: an int number of characters printed
- * On error, -1 is returned, and errno is set appropriately.
- */
-#include "main.h"
-
+* _printf - a function that prints everything
+* @format: the string to print
+* Return: an int number of characters printed
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _printf(const char *format, ...)
 {
-	int char_print, str_length;
-	char c;
-	char *str;
-	va_list l;
+va_list args;
+va_start(args, format);
 
-	str_length = 0;
+int count = 0;
 
-	if (format == NULL)
-	{
-		return (-1);
-	}
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+putchar(va_arg(args, int));
+count++;
+break;
+case 's':
+{
+const char *str = va_arg(args, const char *);
 
-	va_start(l, format);
-
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			char_print++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-			{
-				break;
-			}
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				char_print++;
-			}
-			else if (*format == 'c')
-			{
-				c = va_arg(l, int);
-				write(1, &c, 1);
-				char_print++;
-			}
-			else if (*format == 's')
-			{
-				str = va_arg(l, char*);
-				while (str[str_length] != '\0')
-				{
-					str_length++;
-					write(1, str, str_length);
-					char_print += str_length;
-				}
-			}
-		}
-		format++;
-	}
-	va_end(l);
-	return (char_print);
+while (*str != '\0')
+{
+putchar(*str);
+count++;
+str++;
+}
+}
+break;
+case '%':
+putchar('%');
+count++;
+break;
+default:
+putchar('%');
+count++;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
 }
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 
