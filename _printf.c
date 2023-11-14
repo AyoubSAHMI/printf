0c7b778b1a26b6dec66c9b269d2cd45e0f91bb69
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-11-13 17:15:29 -0800

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,61 +1,61 @@
-#include <stdio.h>
 #include "main.h"
 /**
-*_printf - Print formatted output
-*@format: first string.
-*Return: length of printed content
-*/
+ *_printf - Print formatted output
+ *@format: first string.
+ *Return: length of printed content
+ */
 int _printf(const char *format, ...)
 {
-int i, length;
-int decimal, count_number;
-char *str;
-va_list l;
-i = length = 0;
-va_start(l, format);
-if (format == NULL)
-return (-1);
-while (format[i] != '\0')
-{
-if (format[i] == '%')
-{
-if (format[i + 1] == '\0')
-return (-1);
-if (format[i + 1] == 'c')
-{
-_putchar(va_arg(l, int));
-i++;
-}
-if (format[i + 1] == '%')
-{
-_putchar('%');
-i++;
-}
-if (format[i + 1] == 's')
-{
-str = va_arg(l, char*);
-if (str == NULL)
-{
-write(1, "(null)", strlen("(null)"));
-length += strlen("(null)");
-}
-write(1, str, strlen(str));
-length += (strlen(str) - 1);
-i++;
-}
-if (format[i + 1] == 'd' || format[i + 1] == 'i')
-{
-decimal = va_arg(l, int);
-count_number = printNumberWithPutchar(decimal);
-length += count_number;
-i++;
-}
-}
-else
-_putchar(format[i]);
-length++;
-i++;
-}
-va_end(l);
-return (length);
+	int length, str_len;
+	char *str, c;
+	va_list l;
+
+	length = 0;
+	str_len = 0;
+	va_start(l, format);
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			length++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+			{
+				break;
+			}
+			else if (*format == 'c')
+			{
+				c = va_arg(l, int);
+				write(1, &c, 1);
+				length++;
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(l, char*);
+				while (str[str_len] != '\0')
+				{
+					str_len++;
+				}
+				write(1, str, str_len);
+				length += str_len;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				length++;
+			}
+		}
+		format++;
+	}
+	va_end(l);
+	return (length);
 }
--- main.c
+++ main.c
@@ -4,22 +4,22 @@
 int main(void)
 {
 	int len, len2;
-	
-	len = _printf("Let's try to printf a simple sentence.\n");
+
+	len = _printf2("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	_printf("Length:[%d, %i]\n", len, len);
+	_printf2("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);
+	_printf2("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
-	_printf("Character:[%c]\n", 'H');
+	_printf2("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
+	_printf2("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
-	len = _printf("Percent:[%%]\n");
+	len = _printf2("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
+	_printf2("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
+	_printf2("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 	return (0);
 }
