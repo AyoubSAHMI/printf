62ed832bbbf26261bfa7ce7b3fb106f04942ef77
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-11-14 08:34:10 -0800

    New changes

--- _printf.c
+++ _printf.c
@@ -8,44 +8,55 @@ int _printf(const char *format, ...)
 {
 	int i, length, decimal, count_number, string_length;
 	char *str;
+	char *error;
 	va_list l;
 	i = length = 0;
 	va_start(l, format);
 	if (format == NULL)
+	{
 		return (-1);
+	}
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
+			{
 				return (-1);
-			if (format[i + 1] == 'c')
+			}
+			else if (format[i + 1] == 'c')
 			{
 				_putchar(va_arg(l, int));
 				i++;
 			}
-			if (format[i + 1] == '%')
+			else if (format[i + 1] == '%')
 			{
 				_putchar('%');
 				i++;
 			}
-			if (format[i + 1] == 's')
+			else if (format[i + 1] == 's')
 			{
 				str = va_arg(l, char*);
 				string_length = Sprintf(str);
 				length += string_length;
 				i++;
 			}
-			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
 			{
 				decimal = va_arg(l, int);
 				count_number = printNumberWithPutchar(decimal);
 				length += (count_number - 1);
 				i++;
 			}
+			else
+			{
+				error = "unIdentified formatter\n";
+				write(1, error, str_length(error));
+				return (-1);
+			}
 		}
 		else
-			_putchar(format[i]);
+		_putchar(format[i]);
 		length++;
 		i++;
 	}
