a284d30de31efa2639aaca06bb95c79e3a443311
HajarElMannani <laso125@hotmail.com>
2023-11-14 00:48:47 -0800

    test

--- _printf.c
+++ _printf.c
@@ -6,55 +6,55 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
-
-    int count = 0; // to count the number of characters printed
-    char c;
-
-    while (*format)
-      {
-        if (*format == '%' && *(format + 1) != '\0')
-	  {
-            format++; // move past '%'
-            switch (*format)
-	      {
-                case 'c':
-                    c = (char) va_arg(args, int);
-                    _putchar(c);
-                    count++;
-                    break;
-                case 's':
-                    {
-                        const char *str = va_arg(args, const char *);
-                        while (*str) {
-                           _putchar(*str);
-                            str++;
-                            count++;
-                        }
-                    }
-                    break;
-                case '%':
-                    _putchar('%');
-                    count++;
-                    break;
-                default:
-                    _putchar('%');
-                    _putchar(*format);
-                    count += 2;
-                    break;
-            }
-        }
-	else
-	  {
-            _putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-
-    return (count);
+int i, length;
+int decimal, count_number;
+char *str;
+va_list l;
+i = length = 0;
+va_start(l, format);
+if (format == NULL)
+return (-1);
+while (format[i] != '\0')
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '\0')
+return (-1);
+if (format[i + 1] == 'c')
+{
+_putchar(va_arg(l, int));
+i++;
+}
+if (format[i + 1] == '%')
+{
+_putchar('%');
+i++;
+}
+if (format[i + 1] == 's')
+{
+str = va_arg(l, char*);
+if (str == NULL)
+{
+write(1, "(null)", strlen("(null)"));
+length += strlen("(null)");
+}
+write(1, str, strlen(str));
+length += (strlen(str) - 1);
+i++;
+}
+if (format[i + 1] == 'd' || format[i + 1] == 'i')
+{
+decimal = va_arg(l, int);
+count_number = printNumberWithPutchar(decimal);
+length += count_number;
+i++;
+}
+}
+else
+_putchar(format[i]);
+length++;
+i++;
+}
+va_end(l);
+return (length);
 }
